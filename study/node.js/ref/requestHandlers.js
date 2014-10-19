8cae8c5d1edad57fde7bb28e5dca0437d9c4067d
WooGenius <woogenius@nhnnext.org>
2014-10-19 21:22:58 +0900

    nodejs study file uploaded

---
+++ study/node.js/ref/requestHandlers.js
@@ -0,0 +1,36 @@
+var querystring = require("querystring"),
+    fs = require("fs");
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '이름을 입력하세요' + '<br>' +
+    '<form action="/hello" method = "post">' +
+    '<input type="text" name="text"></input>'+
+    '<input type="submit" value="입력" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function hello(response, postData) {
+  console.log("Request handler 'hello' was called.");
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("안녕하세요. "+   querystring.parse(postData).text + "님"); 
+    console.log(querystring.parse(postData));
+    response.end();
+}
+
+exports.start = start;
+exports.hello = hello;
