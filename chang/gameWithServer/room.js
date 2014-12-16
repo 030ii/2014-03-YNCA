e4a1f2b0cb788e0fdb12460c2396cb89b947ed10
030ii <lovelygeee2@gmail.com>
2014-12-16 17:02:54 +0900

    changFolder and ourServer

---
+++ chang/gameWithServer/room.js
@@ -0,0 +1,143 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/game.html');
+});
+
+var roomIdx = 1;
+
+io.on('connection', function(socket){
+	socket.on('join', function(){
+		socket.join('room' + roomIdx);
+
+		var playerNum = Object.size(io.sockets.adapter.rooms['room'+roomIdx]);	// 특정 방에 들어온 플레이어 수
+
+		socket.emit('join', 'Welcome to room' + roomIdx);		
+		//console.log(playerNum + '명 입장!');									// 한 방에 들어간 플레이어 수
+		//console.log(io.sockets.adapter.rooms['room'+ roomIdx]);	// 그 방에 있는 플레이어들의 id
+
+
+//		io.sockets.in('room'+roomIdx).emit('gameMessage', '게임을 시작합니다.');
+
+
+		/* 한 room에서 플레이어 수가 2명이 되면 (한 방에 2명이 들어가면) */
+		if (playerNum === 2){
+			console.log('room'+ roomIdx + ' 게임 시작');
+			gameStart('room' + roomIdx);
+			roomIdx++; // 방 번호를 증가하여 새로운 방 생성
+			
+
+
+//			io.sockets.in('room'+roomIdx).emit('gameMessage', '게임을 시작합니다.'); //에러는 없지만 안나옴
+//에러		io.socket.in('room'+roomIdx).emit('gameMessage', '게임을 시작합니다.');
+		}
+		
+//		io.sockets.in('room'+roomIdx).emit('gameMessage', '게임을 시작합니다.');
+
+		// socket.leave('room'+roomIdx);
+		// console.log(io.sockets.adapter.rooms['room'+roomIdx]);
+	});		
+
+	socket.on('input', function(inputNum){
+		// ~~
+		console.log(inputNum + '으로 input 받았어요~');
+	});
+
+	// 새로고침 할 때 일어남
+	socket.on('disconnect', function(){
+		console.log(io.sockets.adapter.rooms['room'+roomIdx]);
+		console.log('방 나감');
+	});
+
+});
+
+/* 오브젝트 길이 구하는 함수 (플레이어 수 구하는 함수)
+ * io.sockets.adapter.rooms['room'+roomIdx]는 오브젝트 형태로 보여지기 때문에
+ * 몇 개의 소켓이 접속했는지 알기 위해 (몇 명이 접속했는지 알기 위해)
+ */
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+/* 게임을 시작하는 함수 */
+function gameStart(theRoom){
+
+	io.sockets.in(theRoom).emit('gameMessage', 'Lets play BNW game'); // 안댐
+	
+	console.log('gameStart함수 들어옴');
+	
+	var roomObj = io.sockets.adapter.rooms[theRoom];
+
+	// for (var player in roomObj){
+	// 	// 플레이어 1, 2 생성 및 초기화
+	// }
+
+	// 게임 initialize
+	// 게임 setFirstPlayer -> 선공/후공 알림
+
+	// 선공에게
+	// input하시오 메세지 출력
+	// input 받기 / input 에러 체크 (0~99) 후 input 완료 메세지 출력 
+
+	// 게임 calculate (black/white , 숫자범위)
+
+	// 후공에게
+	// input하시오 메세지 출력
+	// input 받기 / input 에러 체크 (0~99) 후 input 완료 메세지 출력 
+
+	// 게임 calculate (black/white , 숫자범위)
+};
+
+
+// /* 먼저 시작하는 플레이어를 알려주는 함수
+//  * 1 라운드에는 랜덤으로,
+//  * 2~9 라운드에는 이전 승리자가 먼저 시작하는 플레이
+//  */
+// setFirstPlayer : function(){
+// 	if (this.round == 1) {	// 1 라운드
+// 		var random = Math.floor(Math.random() * 2);	// 0~1 랜덤
+
+// 		if (random == 1){
+// 			this.firstPlayer = this.player1;
+// 			this.secondPlayer = this.player2;
+// 			}
+// 		else { // random == 0
+// 			this.firstPlayer = this.player2;
+// 			this.secondPlayer = this.player1;
+// 		}
+// 	}
+// 	else {	// 2~9 라운드 
+// 		this.firstPlayer = this.winner; // winner에는 이전 승리자가 저장되어 있으므
+// 		this.secondPlayer = this.loser;	
+// 	}
+
+// 	console.log(this.firstPlayer.name + "님 숫자를 입력해주세요.");	// 선공
+// };
+
+http.listen(3000, function(){
+	console.log('hello 3000');
+});
+
+
+// 라운드 별로 어떤 정보를 주고 받는지 * 콘솔로 찍기
+
+/* 서버 콘솔찍을거 
+1. room1 게임 시작
+2. 당신은 선공/후공 입니다.
+3. 선공은 input하세요. -- timer -- (후공 입력 불가능)
+4. 선공 input완료! 
+5. 선공은 black/white이고, 범위는 ~~입니다.
+6. 후공은 input하세요. -- timer -- (선공 입력 불가능)
+7. 후공 input완료!
+8. 후공은 black/white이고, 범위는 ~~입니다.
+9. 이번 라운드 승자는 ~~입니다.
+*/
---
+++ ourServer/ourServer.js
@@ -0,0 +1,231 @@
+var path = require('path');
+var session = require('express-session');
+var express = require('express');
+var gameLibrary = require('./gameLogic.js');
+var gameObject = gameLibrary.initialize();
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// 게임 로직 흐름도 - 참고용
+// start() -> getFirstPlayer() -> inputPoint(player, point)*2 
+//         -> isFinished() -> getRoundInfo() -> proceedRound() 
+//         -> getFirstPlayer() -> inputPoint(player, point)*2 
+//         -> ... -> over()
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var gameResource = {
+  INIT_TIME : 60,
+  MAX_ROOM_NUM : 20,
+  isPlayer1 : true,
+  tempRoom : null,
+  rooms : [],
+  
+  initRooms : function () {
+    for (var i = 1; i <= this.MAX_ROOM_NUM; i++) {
+      this.rooms.push({roomName : 'room' + i});
+    }
+  }
+};
+
+gameResource.initRooms();
+
+var tempSocketId;
+
+io.on('connection', function (socket) {
+  
+  /* 플레이어 이름 입력받으면, 2명씩 방에 넣어주는 부분 - 예전 코드랑 똑같다 */
+	socket.on('inputPlayerName', function(playerName){
+    if(gameResource.isPlayer1 === true){
+      socket.room = gameResource.rooms.pop();
+      socket.player = gameObject.player1;
+      socket.player.name = playerName;
+      socket.player.socketId = socket.id;
+      
+      tempRoom = socket.room;
+      socket.join(socket.room.roomName);
+      
+      console.log(playerName + 'join' + socket.room.roomName);
+      
+      socket.emit('Player 2 Waiting...');
+      
+      gameResource.isPlayer1 = false;
+    }
+    else {
+      if(!io.sockets.adapter.rooms[tempRoom.roomName]) {
+        gameResource.isPlayer1 = true;
+        socekt.emit('counterDisconnected');
+        return;
+      }
+      
+      socket.room = tempRoom;
+      socket.player = gameObject.player2;
+      socket.player.name = playerName;
+      socket.player.num = 2;
+      socket.player.socketId = socket.id;
+      
+      gameObject.initializePlayerSocketId(tempSocketId, socket.id);
+      socket.join(socket.room.roomName);
+      
+      socket.broadcast.to(socket.room.roomName).emit('player2 Connect');
+      
+      console.log(playerName + 'join' + socket.room.roomName);
+      
+      gameResource.isPlayer1 = true; 
+    }
+	});
+  
+  socket.on('sendStart', function() {
+    var firstPlayer = gameObject.getFirstPlayer();
+    
+    /* 게임을 시작하는 곳 */
+    io.sockets.in(socket.room.roomName).emit('gameStart', gameObject.start());
+    
+    /* 각 라운드의 선공과 후공을 세팅하는 곳 */
+    settingFirstPlayer(firstPlayer);
+
+  });
+  
+  socket.on('inputPoint', function (point) {
+    
+    if(socket.player !== gameObject.getFirstPlayer())
+      return;
+    
+    /* 게임 로직에서 처리할 듯 */
+    // if(gameObject.isValidPoint(socket.player, point)) {
+    //   socket.emit('invalidPointInput');
+    //   return;
+    // }
+    
+    /* 선공 플레이어 오브젝트를 gameObject 함수의 인자로 넘겨준다 */
+    gameObject.inputPoint(socket.player, point);
+    
+    /* 타이머를 멈춘다 */
+    clearInterval(socket.room.timer);
+    
+    /* 상대 플레이어에게 현재 플레이어의 정보를 넘겨준다 */
+    
+    /* 포인트 게이지를 업데이트 / 포인트 입력창을 Block 처리한다 */
+    socket.emit('updatePointGaugeANDBlockPointInput', socket.player.point);
+    
+    /* 포인트 입력 플레이어가 선공일 때 */
+    if (gameObject.isFirstPlayer(socket.player)) {
+      // 후공을 선정해주고, 후공을 세팅해주는 함수를 호출한다. -인자로 후공 플레이어를 넘겨준다.
+      settingSecondPlayer(gameObject.player.opponent);
+    }
+    else {  /* 포인트 입력 플레이어가 후공일 때 */
+      // 게임이 안 끝났으면, 정보를 보낸다.
+      
+      // 게임 로직 추가할 것 --> gameObject.whoIsWinner(); 위너 판단하는 함수가 있다고 가정했음
+      // 위너가 누군지 판단한다
+      var winner = gameObject.whoIsWinner();
+
+      // 게임이 끝났으면, 위너를 리턴한다.
+      var isFinish = gameObject.isFinished();
+
+      // 한 라운드가 끝났다면,
+      if (!isFinish){
+        sendRoundResult(winner, gameObject.round++);
+
+        wait2SecondsAndProceedRound(gameObject.getRoundInfo);
+      // 비겼으면,
+      } else if (isFinish == 'D') {
+        io.sockets.in(socket.room.roomName).emit('Draw');
+        gameObject.initializeDrawGame();
+
+        var drawFirstPlayer = gameObject.getFirstPlayer();
+        
+        // 2초 후에 시작
+        setTimeout(function() {
+          io.sockets.in(socket.room.roomName).emit('DrawStart', gameObject.getRoundInfo());
+
+          settingFirstPlayer(drawFirstPlayer);
+
+        },2000);
+
+      // 게임이 끝났다면,
+      } else {
+        io.to(isFinish.socketId).emit('gameOverWinner', isFinish.name);
+        io.to(isFinish.opponent.socketId).emit('gameOverLoser', isFinish.opponent.name);
+      }
+
+
+
+    }
+  });
+  
+  /* 플레이어 중 한명이 서버를 나갈 시에 수행되는 부분 */
+  socket.on('disconnect', function () {
+    console.log('someone is disconnected.');
+  });
+  
+  /* 선공 세팅 */
+  /* sendStart 가 on 되고, 각 라운드의 선공/후공 세팅하는 부분에서,
+  선공을 세팅하는 함수를 호출하면 실행되는 곳 */
+  function settingFirstPlayer(firstPlayer) {
+    io.to(firstPlayer.socketId).emit('firstPlayerSetting');
+    io.to(firstPlayer.socketId).emit('showModal', 'It`s your turn. Input the Point.');
+    
+    io.sockets.in(socket.room.roomName).emit('setRemainingTime', gameResource.INIT_TIME);
+    
+    this.settingTimer(firstPlayer);
+    
+    io.to(firstPlayer.opponenet.socketId).emit('SecondPlayerSetting');
+ 
+  }
+
+  /* 후공 세팅 */
+  function settingSecondPlayer(secondPlayer) {
+    io.to(secondPlayer.socketId).emit('showModal', 'It`s your turn. Input the Point.');
+    io.to(secondPlayer.socketId).emit('deleteBlock');
+
+    this.settingTimer(secondPlayer);
+  }
+  
+  /* 라운드마다 각 플레이어의 타이머를 설정하는 함수 */
+  function settingTimer(player){
+    socket.room.remainingTime = gameResource.INIT_TIME;
+    socket.room.timer = setInterval(function(){
+      if(socket.room !== null) {
+        clearInterval(socket.room.timer);
+        return;
+      }
+      
+      if(socket.room.remainingTime >= 0) {
+        io.to(player.socketId).emit('setRemainigTime', socket.room.remainingTime);
+      }
+      else {
+        io.to(player.socketId).emit('inputTimeout');
+        clearInterval(socket.room.timer);
+      }
+      socket.room.remainingTime -= 1;
+    }, 1000);
+  }
+  
+  /* 라운드가 끝난 뒤, 라운드의 정보를 보내는 함수 */
+  function sendRoundResult (winner, round){
+    io.to(winner.socketId).emit('youAreWinner',round + '라운드 결과 : 승리');
+    io.to(winner.opponent.socketId).emit('youAreLoser',round + '라운드 결과 : 패배');
+  }
+ 
+  /* 라운드가 끝난 뒤, 다음 라운드를 진행하는 함수 */
+  function proceedRound(roundInfo, game){
+    
+  }
+
+  /* 2초를 기다리고 라운드를 진행하는 함수 */
+  function wait2SecondsAndProceedRound(roundInfo){
+    setTimeout(function(){
+      io.sockets.in(socket.room.roomName).emit('nextRound', roundInfo);
+
+      var firstPlayer = gameObject.getFirstPlayer();
+      settingFirstPlayer(firstPlayer);
+    },2000);
+  }
+});
+
+http.listen(3000, function () {
+	console.log('Express server listening on port 3000');
+});
