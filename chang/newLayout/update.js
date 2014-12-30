090e408d95b8014df88fbce3aac3551c5711d763
030ii <lovelygeee2@gmail.com>
2014-12-30 18:19:59 +0900

    flip효과 추가, update.js 추가
    update.js
    - function updateNotiMessage
    - function updateRound
    - function updateBNW (상대방인지 나인지 구별하는 오브젝트 필요함. 일단 한국어로 씀)
    - function updateBNWbyRound (상대방인지 나인지 구별하는 오브젝트 필요함. 일단 한국어로 씀)

---
+++ chang/newLayout/update.js
@@ -0,0 +1,47 @@
+function updateNotiMessage(message){
+	document.getElementById("notiMessage").innerHTML = message;
+}
+
+function updateRound(round){
+	document.getElementById("roundNum").innerHTML = round;
+}
+
+function updateBNW(ele, color) {
+	if (ele == '상대방'){
+		if (color =='B') {
+			document.getElementById('urColor').style.backgroundColor = "black";
+		}
+		else {
+			document.getElementById('urColor').style.backgroundColor = "white";
+		}
+	}
+
+	if (ele == '나'){
+		if (color =='B') {
+			document.getElementById('myColor').style.backgroundColor = "black";
+		}
+		else {
+			document.getElementById('myColor').style.backgroundColor = "white";
+		}
+	}
+}
+
+function updateBNWbyRound(ele, round, color) {
+	if (ele == '상대방'){
+		if (color =='B') {
+			document.querySelector('#urBNWbyRound td:nth-child(' + round + ')').style.backgroundColor  = "black";
+		}
+		else {
+			document.querySelector('#urBNWbyRound td:nth-child(' + round + ')').style.backgroundColor  = "white";
+		}
+	}
+
+	if (ele == '나'){
+		if (color =='B') {
+			document.querySelector('#myBNWbyRound td:nth-child(' + round + ')').style.backgroundColor  = "black";
+		}
+		else {
+			document.querySelector('#myBNWbyRound td:nth-child(' + round + ')').style.backgroundColor  = "white";
+		}
+	}
+}
